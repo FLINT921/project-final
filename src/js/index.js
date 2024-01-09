73807e5d0fa7307a0eead009de6df01cf0ae9f54
FLINT921 <roman.alex9009@gamil.com>
2024-01-09 20:22:52 +0300

    last commit 09.01/20.22

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 import '../scss/style.scss'
 import '../scss/fonts.scss'
+import '../scss/about.scss'
+import '../scss/burger-menu.scss'
+import '../scss/footer.scss'
+import '../scss/nav.scss'
+import '../scss/right-blocks.scss'
+import '../scss/slide-menu.scss'
+import '../scss/wrapper-blocks.scss'
 
 import './swiper.js'
 
